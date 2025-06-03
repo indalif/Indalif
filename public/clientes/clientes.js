db6889b59152b2d52f59d074e1f57ee9af5668df
paurodriguez17 <168382321+paurodriguez17@users.noreply.github.com>
2025-06-03 12:18:04 -0300

    Update clientes.js

--- public/clientes/clientes.js
+++ public/clientes/clientes.js
@@ -498,7 +498,7 @@ function cargarMetricasCliente(idCliente, nombreCliente) {
 
             // Crear el gráfico de torta
             window.graficoMetricas = new Chart(ctx, {
-                type: 'doughnut',
+                type: 'pie',
                 data: {
                     labels: ['Mercadería', 'Pagado', 'Cambios'],
                     datasets: [{
